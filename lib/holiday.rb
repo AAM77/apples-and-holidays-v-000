6851ad713ee79362a3106e5718b9d4d32933ea0d
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-25 15:20:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,9 +48,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
       end
     end
   end
-
-  #holiday_hash[:winter][:christmas].push(supply)
-  #holiday_hash[:winter][:new_years].push(supply)
 end
 
 
